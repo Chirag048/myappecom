9111c31cd21a0a174eb842cba91925ddbfcbccb4
Chirag <chirag@Chirags-MacBook-Pro.local>
2022-07-24 01:17:40 +0530

    Login page edit

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,55 +1,133 @@
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String name = '';
+  bool changeButton = false;
+
+  final _formKey = GlobalKey<FormState>();
+
+  moveToHome(BuildContext context) async {
+    if (_formKey.currentState!.validate()) {
+      setState(() {
+        changeButton = true;
+      });
+
+      await Future.delayed(const Duration(seconds: 1));
+
+      if (!mounted) return;
+      await Navigator.pushNamed(context, "/");
+      setState(() {
+        changeButton = false;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Material(
       color: Colors.white,
       child: SingleChildScrollView(
-        child: Column(
-          children: [
-            Image.asset(
-              "assets/images/login_image.png",
-              fit: BoxFit.cover,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                Image.asset(
+                  "assets/images/login_image.png",
+                  fit: BoxFit.cover,
+                ),
+                const SizedBox(
+                  height: 20.0,
+                ),
+                Text(
+                  "Welcome $name",
+                  style: const TextStyle(
+                      fontSize: 22, fontWeight: FontWeight.bold),
+                ),
+                Column(
+                  children: [
+                    TextFormField(
+                      decoration: const InputDecoration(
+                          hintText: "Enter Your Email Or Username",
+                          labelText: "Email Or Username"),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Username cannot be empty";
+                        }
+
+                        return null;
+                      },
+                      onChanged: (value) {
+                        name = value;
+                        setState(() {});
+                      },
+                    ),
+
+                    TextFormField(
+                      decoration: const InputDecoration(
+                          hintText: "Enter Your Password",
+                          labelText: "Password"),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Password cannot be empty";
+                        } else if (value.length < 6) {
+                          return "Password length should be atleast 6";
+                        }
+
+                        return null;
+                      },
+                      obscureText: true,
+                    ),
+                    const SizedBox(
+                      height: 40.0,
+                    ),
+
+                    Material(
+                      color: Colors.deepPurple,
+                      borderRadius:
+                          BorderRadius.circular(changeButton ? 50 : 8),
+                      child: InkWell(
+                        borderRadius: BorderRadius.circular(8),
+                        onTap: () => moveToHome(context),
+                        child: AnimatedContainer(
+                          duration: const Duration(seconds: 1),
+                          width: changeButton ? 50 : 150,
+                          height: 50,
+                          alignment: Alignment.center,
+                          child: changeButton
+                              ? const Icon(
+                                  Icons.done,
+                                  color: Colors.white,
+                                )
+                              : const Text("Login",
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 18)),
+                        ),
+                      ),
+                    )
+                    // ElevatedButton(
+                    //   onPressed: () {
+                    //     Navigator.pushNamed(context, "/");
+                    //   },
+                    //   child: const Text("Login"),
+                    //   style:
+                    //       TextButton.styleFrom(minimumSize: const Size(100, 40)),
+                    // )
+                  ],
+                )
+              ],
             ),
-            const SizedBox(
-              height: 20.0,
-            ),
-            const Text(
-              "Welcome",
-              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                children: [
-                  TextFormField(
-                    decoration: const InputDecoration(
-                        hintText: "Enter Your Email Or Username",
-                        labelText: "Email Or Username"),
-                  ),
-                  TextFormField(
-                    decoration: const InputDecoration(
-                        hintText: "Enter Your Password", labelText: "Password"),
-                    obscureText: true,
-                  ),
-                  const SizedBox(
-                    height: 40.0,
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, "/");
-                    },
-                    child: const Text("Login"),
-                    style:
-                        TextButton.styleFrom(minimumSize: const Size(100, 40)),
-                  )
-                ],
-              ),
-            )
-          ],
+          ),
         ),
       ),
     );
